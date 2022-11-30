4401d1b8a28dcc9fcef3b108c3804d522d497530
Talescruzs <tcs2017@hotmail.com>
2022-11-30 20:50:54 -0300

    inicio da parte visual

---
+++ carrinho/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="Item">
+          <p>nome</p>
+          <p>valor</p>
+        </div>
+        <div className="Sum">
+          <p>valor total:</p>
+          <p>valor</p>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
